338270af99d5c397b123805bcc06c81f82f037c3
Gustavo <ottoguz87@gmail.com>
2023-10-17 12:41:47 -0300

    adds dynamic recipes list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'Test Desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
     new Recipe('A test Recipe', 'Test Desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 }
